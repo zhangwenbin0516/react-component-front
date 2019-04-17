90a76c66ed248442db515502f4b2f214aec72a68
zhangwenbin <942623159@qq.com>
2019-04-17 18:45:43 +0800

    's'

--- src/redux/index.jsx
+++ src/redux/index.jsx
@@ -1,16 +1,9 @@
 import {createStore} from "redux";
 import Msgsnd from './reducers'
 
-console.log(Msgsnd)
+console.log()
 const TestData = (state = {}, action) => {
-    switch (action.type) {
-        case 'INCREMENT':   //增加数据
-            one('www');
-        case 'DECREMENT':   //删除数据
-            two('llll');
-        default:
-            return state;
-    }
+    Msgsnd[action.type]();
 }
 
 let store = createStore(TestData)
