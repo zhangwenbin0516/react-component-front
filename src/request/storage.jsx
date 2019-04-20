d1f992e9e8d059b48f847af47d8c0d291b4f4a09
ZhangWenbin <942623159@qq.com>
2019-04-21 03:59:19 +0800

    'dd'

--- src/request/storage.jsx
+++ src/request/storage.jsx
@@ -1,59 +1,64 @@
 "use strict";
-let StorageData = new Object();
-
-StorageData.getDate = function () {
-    this.getTime = new Date().getTime();
-    this.updateTime = this.getTime + 30 * 60 * 1000;
-    this.timing = this.getTime + 3 * 60 * 1000;
-    let time = localStorage.getItem('timing');
-    if (this.updateTime <= this.getTime) {
-        this.clearData()
+class CreateComponent {
+    constructor() {
+        this.getDate()
     }
-    if (time) {
-        if (this.getTime > time) {
+    async getDate() {
+        this.getTime = new Date().getTime();
+        this.updateTime = this.getTime + 30 * 60 * 1000;
+        this.timing = this.getTime + 3 * 60 * 1000;
+        let time = localStorage.getItem('timing');
+        if (this.updateTime <= this.getTime) {
+            this.clearData()
+        }
+        if (time) {
+            if (this.getTime > time) {
+                localStorage.setItem('timing', this.timing)
+                localStorage.setItem('updateTime', this.updateTime)
+            }
+        } else {
             localStorage.setItem('timing', this.timing)
             localStorage.setItem('updateTime', this.updateTime)
         }
-    } else {
-        localStorage.setItem('timing', this.timing)
-        localStorage.setItem('updateTime', this.updateTime)
+    }
+
+    async setData(key, data) {
+        this.getDate();
+        let lists = {
+            startTime: this.getTime,
+            data: data
+        }
+        localStorage.setItem(key, JSON.stringify(lists));
+    }
+
+    getData(key) {
+        let data = localStorage.getItem(key);
+        if (data) {
+            let lists = JSON.parse(data);
+            return lists.data
+        }
+    }
+
+    async updateData(item, key, data) {
+        this.getDate();
+        let body = localStorage.getItem(item);
+        if (body) {
+            let lists = JSON.parse(body);
+            lists['updateTime'] = this.getTime;
+            lists[key] = data;
+            localStorage.setItem(item, JSON.stringify(lists));
+        }
+    }
+
+    async removeData(key) {
+        await localStorage.removeItem(key)
+    }
+
+    async clearData() {
+        await localStorage.clear()
     }
 }
 
-StorageData.getData = function (key) {
-    let data = localStorage.getItem(key);
-    if (data) {
-        let lists = JSON.parse(data);
-        return lists.data
-    }
-}
-
-StorageData.setData = function (key, data) {
-    this.getDate();
-    let lists = {
-        startTime: this.getTime,
-        data: data
-    }
-    localStorage.setItem(key, JSON.stringify(lists));
-}
-
-StorageData.updateData = async function (item, key, data) {
-    this.getDate();
-    let body = localStorage.getItem(item);
-    if (body) {
-        let lists = JSON.parse(body);
-        lists['updateTime'] = this.getTime;
-        lists[key] = data;
-        localStorage.setItem(item, JSON.stringify(lists));
-    }
-}
-
-StorageData.removeData = function (key) {
-    localStorage.removeItem(key)
-}
-
-StorageData.clearData = function () {
-    localStorage.clear()
-}
+const StorageData = new CreateComponent()
 
 export default StorageData
